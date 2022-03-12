74180a572d75a33b6643f45b56cd7993dc1fd931
akm <akm2000@gmail.com>
2022-03-12 23:39:02 +0900

    :green_heart: :recycle: Define run func in TestUnit and TestInterfaceSection

--- parser/unit_test.go
+++ parser/unit_test.go
@@ -8,60 +8,49 @@ import (
 )
 
 func TestUnit(t *testing.T) {
-	type pattern struct {
-		name     string
-		text     []rune
-		expected *ast.Unit
-	}
+	run := func(name string, text []rune, expected *ast.Unit) {
+		t.Run(name, func(t *testing.T) {
+			parser := NewParser(&text)
+			res, err := parser.ParseUnit()
+			if assert.NoError(t, err) {
+				assert.Equal(t, expected, res)
+			}
 
-	patterns := []*pattern{
-		{
-			// "unit" is loaded already in goal.go
-			name: "simplest unit",
-			text: []rune(`U1;
+		})
+	}
+	// "unit" is loaded already in goal.go
+	run(
+		"simplest unit",
+		[]rune(`U1;
 			interface
 			implementation
 			end.`),
-			expected: &ast.Unit{
-				Ident:                 ast.Ident("U1"),
-				InterfaceSection:      &ast.InterfaceSection{},
-				ImplementationSection: &ast.ImplementationSection{},
-			},
+		&ast.Unit{
+			Ident:                 ast.Ident("U1"),
+			InterfaceSection:      &ast.InterfaceSection{},
+			ImplementationSection: &ast.ImplementationSection{},
 		},
-	}
-
-	for _, ptn := range patterns {
-		parser := NewParser(&ptn.text)
-		res, err := parser.ParseUnit()
-		if assert.NoError(t, err) {
-			assert.Equal(t, ptn.expected, res)
-		}
-	}
+	)
 }
 
 func TestInterfaceSection(t *testing.T) {
-	type pattern struct {
-		name     string
-		text     []rune
-		expected *ast.InterfaceSection
+	run := func(name string, text []rune, expected *ast.InterfaceSection) {
+		t.Run(name, func(t *testing.T) {
+			parser := NewParser(&text)
+			res, err := parser.ParseInterfaceSection()
+			if assert.NoError(t, err) {
+				assert.Equal(t, expected, res)
+			}
+
+		})
 	}
 
-	patterns := []*pattern{
-		{
-			// "unit" is loaded already in goal.go
-			name: "Uses only",
-			text: []rune(`USES U1,U2,U3;`),
-			expected: &ast.InterfaceSection{
-				UsesClause: &ast.UsesClause{"U1", "U2", "U3"},
-			},
+	// "unit" is loaded already in goal.go
+	run(
+		"Uses only",
+		[]rune(`USES U1,U2,U3;`),
+		&ast.InterfaceSection{
+			UsesClause: &ast.UsesClause{"U1", "U2", "U3"},
 		},
-	}
-
-	for _, ptn := range patterns {
-		parser := NewParser(&ptn.text)
-		res, err := parser.ParseInterfaceSection()
-		if assert.NoError(t, err) {
-			assert.Equal(t, ptn.expected, res)
-		}
-	}
+	)
 }
