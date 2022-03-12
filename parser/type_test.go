3da5d97e9d070bdaa9917b9372a96a8093379f14
akm <akm2000@gmail.com>
2022-03-12 23:19:03 +0900

    :green_heart: Add TestUnitWithTypeSection

--- parser/type_test.go
+++ parser/type_test.go
@@ -7,6 +7,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUnitWithTypeSection(t *testing.T) {
+	run := func(name string, text []rune, expected *ast.Unit) {
+		t.Run(name, func(t *testing.T) {
+			parser := NewParser(&text)
+			parser.NextToken()
+			res, err := parser.ParseUnit()
+			if assert.NoError(t, err) {
+				assert.Equal(t, expected, res)
+			}
+		})
+	}
+
+	u2 := ast.UnitId("U2")
+	u4 := ast.UnitId("U4")
+	run(
+		"2 type declarations",
+		[]rune(`
+		UNIT U1;
+		INTERFACE
+		TYPE
+			TTypeId1 = TType1;
+			TTypeId2 = U2.TType2;
+		TYPE TTypeId3 = TType3;
+		TYPE TTypeId4 = U4.TType4;
+		IMPLEMENTATION
+		END.`),
+		&ast.Unit{
+			Ident: ast.Ident("U1"),
+			InterfaceSection: &ast.InterfaceSection{
+				InterfaceDecls: []ast.InterfaceDecl{
+					ast.TypeSection{
+						{Ident: ast.Ident("TTypeId1"), Type: &ast.TypeId{Ident: ast.Ident("TType1")}},
+						{Ident: ast.Ident("TTypeId2"), Type: &ast.TypeId{UnitId: &u2, Ident: ast.Ident("TType2")}},
+					},
+					ast.TypeSection{
+						{Ident: ast.Ident("TTypeId3"), Type: &ast.TypeId{Ident: ast.Ident("TType3")}},
+					},
+					ast.TypeSection{
+						{Ident: ast.Ident("TTypeId4"), Type: &ast.TypeId{UnitId: &u4, Ident: ast.Ident("TType4")}},
+					},
+				},
+			},
+			ImplementationSection: &ast.ImplementationSection{},
+		},
+	)
+}
+
 func TestTypeSection(t *testing.T) {
 	run := func(name string, text []rune, expected ast.TypeSection) {
 		t.Run(name, func(t *testing.T) {
