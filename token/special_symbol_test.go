73ea74668cade37de9876ac12290b3be2195f97e
akm <akm2000@gmail.com>
2022-03-13 17:22:29 +0900

    :sparkles: Add token.ProcessDoubleSpecialSymbol

---
+++ token/special_symbol_test.go
@@ -0,0 +1,45 @@
+package token_test
+
+import (
+	"testing"
+
+	"github.com/akm/tparser/token"
+)
+
+func TestSpecialSymbolDots(t *testing.T) {
+	patterns := TestPatterns{
+		{
+			text: `1.2.3`,
+			tokens: TestTokens{
+				{Type: token.NumeralReal, Content: "1.2"},
+				{Type: token.SpecialSymbol, Content: "."},
+				{Type: token.NumeralInt, Content: "3"},
+			},
+		},
+		{
+			text: `1.2..3.4`,
+			tokens: TestTokens{
+				{Type: token.NumeralReal, Content: "1.2"},
+				{Type: token.SpecialSymbol, Content: ".."},
+				{Type: token.NumeralReal, Content: "3.4"},
+			},
+		},
+		{
+			text: `'A'..'Z'`,
+			tokens: TestTokens{
+				{Type: token.CharacterString, Content: "'A'"},
+				{Type: token.SpecialSymbol, Content: ".."},
+				{Type: token.CharacterString, Content: "'Z'"},
+			},
+		},
+		{
+			text: `Green..White`,
+			tokens: TestTokens{
+				{Type: token.Identifier, Content: "Green"},
+				{Type: token.SpecialSymbol, Content: ".."},
+				{Type: token.Identifier, Content: "White"},
+			},
+		},
+	}
+	patterns.check(t)
+}
