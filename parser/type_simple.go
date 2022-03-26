ae7551fca34108c750a831cb62f6c758dda833f9
akm <akm2000@gmail.com>
2022-03-26 16:25:50 +0900

    :recycle: Extract type_simple.go and type_string.go

---
+++ parser/type_simple.go
@@ -0,0 +1,113 @@
+package parser
+
+import (
+	"github.com/akm/tparser/ast"
+	"github.com/akm/tparser/token"
+	"github.com/pkg/errors"
+)
+
+var realType = token.PredicatorBy("RealType", ast.IsRealTypeName)
+
+func (p *Parser) ParseRealType(required bool) (*ast.RealType, error) {
+	t := p.CurrentToken()
+	if t.Is(realType) {
+		p.NextToken()
+		return &ast.RealType{Name: ast.Ident(t.Value())}, nil
+	} else if required {
+		return nil, errors.Errorf("Unsupported token %+v for RealType", t)
+	} else {
+		return nil, nil
+	}
+}
+
+var ordIdent = token.PredicatorBy("OrdIdent", ast.IsOrdIdentName)
+
+func (p *Parser) ParseOrdIdent(required bool) (*ast.OrdIdent, error) {
+	t := p.CurrentToken()
+	if t.Is(ordIdent) {
+		p.NextToken()
+		return &ast.OrdIdent{Name: ast.Ident(t.Value())}, nil
+	} else if required {
+		return nil, errors.Errorf("Unsupported token %+v for OrdIdent", t)
+	} else {
+		return nil, nil
+	}
+}
+
+// t1 must be identifier token
+// t2 can be ".." or others
+func (p *Parser) parseSubrangeTypeForIdentifier(t1, t2 *token.Token, required bool) (*ast.SubrangeType, error) {
+	if t2.Is(token.Value("..")) {
+		p.NextToken()
+		expr, err := p.ParseConstExpr()
+		if err != nil {
+			return nil, err
+		}
+		return &ast.SubrangeType{
+			Low:  *ast.NewConstExpr(t1.Value()),
+			High: *expr,
+		}, nil
+	} else if required {
+		return nil, errors.Errorf("Unsupported token %+v, %+v for SubrangeType", t1, t2)
+	} else {
+		return nil, nil
+	}
+}
+
+func (p *Parser) ParseConstSubrageType() (*ast.SubrangeType, error) {
+	lowExpr, err := p.ParseConstExpr()
+	if err != nil {
+		return nil, err
+	}
+	if _, err := p.Current(token.Value("..")); err != nil {
+		return nil, err
+	}
+	p.NextToken()
+	highExpr, err := p.ParseConstExpr()
+	if err != nil {
+		return nil, err
+	}
+	return &ast.SubrangeType{
+		Low:  *lowExpr,
+		High: *highExpr,
+	}, nil
+}
+
+func (p *Parser) ParseEnumeratedType() (ast.EnumeratedType, error) {
+	res := ast.EnumeratedType{}
+	for {
+		element, err := p.ParseEnumeratedTypeElement()
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, element)
+		t := p.CurrentToken()
+		if t.Is(token.Symbol(')')) {
+			break
+		} else if t.Is(token.Symbol(',')) {
+			continue
+		} else {
+			return nil, errors.Errorf("Unsupported token %+v for EnumeratedType", t)
+		}
+	}
+	return res, nil
+}
+
+func (p *Parser) ParseEnumeratedTypeElement() (*ast.EnumeratedTypeElement, error) {
+	ident, err := p.Next(token.Identifier)
+	if err != nil {
+		return nil, err
+	}
+	res := &ast.EnumeratedTypeElement{Ident: ast.Ident(ident.Value())}
+	p.NextToken()
+	if p.CurrentToken().Is(token.Symbol('=')) {
+		p.NextToken()
+		expr, err := p.ParseConstExpr()
+		if err != nil {
+			return nil, err
+		}
+		res.ConstExpr = expr
+	}
+
+	return res, nil
+}
