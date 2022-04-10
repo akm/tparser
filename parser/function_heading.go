feec0931d8b23aa5c3e6619d339059ab9b3273c3
akm <akm2000@gmail.com>
2022-04-10 20:17:10 +0900

    :sparkles: Define skelton methods

---
+++ parser/function_heading.go
@@ -0,0 +1,13 @@
+package parser
+
+import (
+	"github.com/akm/tparser/ast"
+)
+
+func (p *Parser) ParseExportedHeading() (*ast.ExportedHeading, error) {
+	return nil, nil
+}
+
+func (p *Parser) ParseFormalParameters() (*ast.FormalParameters, error) {
+	return nil, nil
+}
