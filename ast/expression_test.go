0061cc48dc33b9b97dd1ca79ac2c7a693e1720a5
akm <akm2000@gmail.com>
2022-04-19 10:17:29 +0900

    :green_heart: Add tests for interface implementations

---
+++ ast/expression_test.go
@@ -0,0 +1,27 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExpression(t *testing.T) {
+	t.Run("Factor", func(t *testing.T) {
+		assert.Implements(t, (*Factor)(nil), &DesignatorFactor{})
+		assert.Implements(t, (*Factor)(nil), &Address{})
+		assert.Implements(t, (*Factor)(nil), &ValueFactor{})
+		assert.Implements(t, (*Factor)(nil), &Number{})
+		assert.Implements(t, (*Factor)(nil), &String{})
+		assert.Implements(t, (*Factor)(nil), &Nil{})
+		assert.Implements(t, (*Factor)(nil), &Parentheses{})
+		assert.Implements(t, (*Factor)(nil), &Not{})
+		assert.Implements(t, (*Factor)(nil), SetConstructor{})
+		assert.Implements(t, (*Factor)(nil), &TypeCast{})
+	})
+	t.Run("DesignatorItem", func(t *testing.T) {
+		assert.Implements(t, (*DesignatorItem)(nil), DesignatorItemIdent(Ident("foo")))
+		assert.Implements(t, (*DesignatorItem)(nil), DesignatorItemExprList{})
+		assert.Implements(t, (*DesignatorItem)(nil), &DesignatorItemDereference{})
+	})
+}
---
+++ ast/type_test.go
@@ -0,0 +1,20 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestType(t *testing.T) {
+	t.Run("Type", func(t *testing.T) {
+		assert.Implements(t, (*Type)(nil), &TypeId{})
+		// assert.Implements(t, (*Type)(nil), &SimpleType{}) // this check is in type_simple_test.go
+		// assert.Implements(t, (*Type)(nil), &StrucType{})
+		// assert.Implements(t, (*Type)(nil), &PointerType{})
+		assert.Implements(t, (*Type)(nil), &StringType{})
+		// assert.Implements(t, (*Type)(nil), &ProcedureType{})
+		// assert.Implements(t, (*Type)(nil), &VariantType{})
+		// assert.Implements(t, (*Type)(nil), &ClassRefType{})
+	})
+}
