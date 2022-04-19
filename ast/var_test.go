2cc3521860d65fccefd4651aa18cd584ce1a87f4
akm <akm2000@gmail.com>
2022-04-19 10:17:29 +0900

    :green_heart: Add tests for interface implementations

---
+++ ast/var_test.go
@@ -0,0 +1,25 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVar(t *testing.T) {
+	t.Run("VarSection implements InterfaceDecl", func(t *testing.T) {
+		var decl InterfaceDecl
+		decl = VarSection{}
+		assert.Implements(t, (*InterfaceDecl)(nil), decl)
+	})
+	t.Run("VarDeclAbsoluteConstExpr implements VarDeclAbsolute", func(t *testing.T) {
+		var abs VarDeclAbsolute
+		abs = &VarDeclAbsoluteConstExpr{}
+		assert.Implements(t, (*VarDeclAbsolute)(nil), abs)
+	})
+	t.Run("ThreadVarSection implements InterfaceDecl", func(t *testing.T) {
+		var decl InterfaceDecl
+		decl = ThreadVarSection{}
+		assert.Implements(t, (*InterfaceDecl)(nil), decl)
+	})
+}
