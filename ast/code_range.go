36e0a96a51b13c76daa263a4dbfa89ec1c0ac6f1
akm <akm2000@gmail.com>
2022-04-21 21:12:38 +0900

    :sparkles: Make CodePosition as an alias to runes.Position
    Position.Index is required to extract code from source file.

--- ast/code_range.go
+++ ast/code_range.go
@@ -1,9 +1,8 @@
 package ast
 
-type CodePosition struct {
-	Line int
-	Col  int
-}
+import "github.com/akm/tparser/runes"
+
+type CodePosition = runes.Position
 
 type CodeRange struct {
 	Path  string
