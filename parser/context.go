d8e29ccf612be0f691b52e423b4b3282e58463a4
akm <akm2000@gmail.com>
2022-05-02 13:46:32 +0900

    :sparkles: Add Units to Context

--- parser/context.go
+++ parser/context.go
@@ -1,21 +1,26 @@
 package parser
 
 import (
+	"github.com/akm/tparser/ast"
 	"github.com/akm/tparser/ext"
 	"github.com/akm/tparser/token"
 	"github.com/pkg/errors"
 )
 
 type Context struct {
-	unitIdentifiers ext.Strings
+	unitIdentifiers ext.Strings // TO BE REMOVED
+	Units           ast.Units
 }
 
 func NewContext(args ...interface{}) *Context {
 	var unitIdentifiers ext.Strings
+	var units ast.Units
 	for _, arg := range args {
 		switch v := arg.(type) {
 		case ext.Strings:
 			unitIdentifiers = v
+		case ast.Units:
+			units = v
 		default:
 			panic(errors.Errorf("unexpected type %T (%v) is given for NewContext", arg, arg))
 		}
@@ -23,11 +28,15 @@ func NewContext(args ...interface{}) *Context {
 	if unitIdentifiers == nil {
 		unitIdentifiers = ext.Strings{}
 	}
+	if units == nil {
+		units = ast.Units{}
+	}
 	return &Context{
 		unitIdentifiers: unitIdentifiers,
+		Units:           units,
 	}
 }
 
 func (c *Context) IsUnitIdentifier(token *token.Token) bool {
-	return c.unitIdentifiers.Include(token.Value())
+	return c.unitIdentifiers.Include(token.Value()) || c.Units.ByName(token.Value()) != nil
 }
