1609ed2054bc8cf13db479172248a3da0cabdcaf
akm <akm2000@gmail.com>
2022-05-05 11:55:21 +0900

    :sparkles: Define DeclarationMap and use it in parser.Context

--- parser/context.go
+++ parser/context.go
@@ -2,6 +2,7 @@ package parser
 
 import (
 	"github.com/akm/tparser/ast"
+	"github.com/akm/tparser/ast/astcore"
 	"github.com/akm/tparser/ext"
 	"github.com/akm/tparser/token"
 	"github.com/pkg/errors"
@@ -10,17 +11,21 @@ import (
 type Context struct {
 	unitIdentifiers ext.Strings // TO BE REMOVED
 	Units           ast.Units
+	DeclarationMap  astcore.DeclarationMap
 }
 
 func NewContext(args ...interface{}) *Context {
 	var unitIdentifiers ext.Strings
 	var units ast.Units
+	var declarationMap astcore.DeclarationMap
 	for _, arg := range args {
 		switch v := arg.(type) {
 		case ext.Strings:
 			unitIdentifiers = v
 		case ast.Units:
 			units = v
+		case astcore.DeclarationMap:
+			declarationMap = v
 		default:
 			panic(errors.Errorf("unexpected type %T (%v) is given for NewContext", arg, arg))
 		}
@@ -31,9 +36,13 @@ func NewContext(args ...interface{}) *Context {
 	if units == nil {
 		units = ast.Units{}
 	}
+	if declarationMap == nil {
+		declarationMap = astcore.NewDeclarationMap()
+	}
 	return &Context{
 		unitIdentifiers: unitIdentifiers,
 		Units:           units,
+		DeclarationMap:  declarationMap,
 	}
 }
 
