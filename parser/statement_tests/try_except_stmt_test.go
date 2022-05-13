4a5169daabf35c03c22f55dbb67183461d15a704
akm <akm2000@gmail.com>
2022-05-14 08:41:29 +0900

    :green_heart: Fix tests for TryExceptStmt

--- parser/statement_tests/try_except_stmt_test.go
+++ parser/statement_tests/try_except_stmt_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/akm/tparser/ast"
+	"github.com/akm/tparser/ast/astcore"
 	"github.com/akm/tparser/ast/asttest"
 )
 
@@ -22,25 +23,19 @@ except
 end;
 `),
 		&ast.Statement{
-			Body: &ast.RepeatStmt{
-				StmtList: ast.StmtList{
+			Body: &ast.TryExceptStmt{
+				Statements: ast.StmtList{
 					{
-						Body: &ast.TryExceptStmt{
-							Statements: ast.StmtList{
-								{
-									Body: &ast.CallStatement{
-										Designator: asttest.NewDesignator("DoSomething"),
-									},
-								},
-							},
-							ExceptionBlock: &ast.ExceptionBlock{
-								Else: ast.StmtList{
-									{
-										Body: &ast.AssignStatement{
-											Designator: asttest.NewDesignator("HandleException"),
-										},
-									},
-								},
+						Body: &ast.CallStatement{
+							Designator: asttest.NewDesignator("DoSomething"),
+						},
+					},
+				},
+				ExceptionBlock: &ast.ExceptionBlock{
+					Else: ast.StmtList{
+						{
+							Body: &ast.CallStatement{
+								Designator: asttest.NewDesignator("HandleException"),
 							},
 						},
 					},
@@ -49,102 +44,159 @@ end;
 		},
 	)
 
-	run(
-		"with 1 ExceptionBlockHandler",
-		[]rune(`
-try
-	X := Y/Z;
-except
-	on EZeroDivide do HandleZeroDivide;
-end;
+	stateToProgram := func(programName string, declSections ast.DeclSections, state *ast.Statement) *ast.Program {
+		return &ast.Program{
+			Ident: asttest.NewIdent(programName),
+			ProgramBlock: &ast.ProgramBlock{
+				Block: func() *ast.Block {
+					return &ast.Block{
+						DeclSections: declSections,
+						CompoundStmt: &ast.CompoundStmt{
+							StmtList: ast.StmtList{
+								state,
+							},
+						},
+					}
+				}(),
+			},
+		}
+	}
+
+	typeDeclEZeroDivide := &ast.TypeDecl{
+		Ident: asttest.NewIdent("EZeroDivide"),
+		Type:  asttest.NewTypeId("Exception"),
+	}
+	typeDeclEOverflow := &ast.TypeDecl{
+		Ident: asttest.NewIdent("EOverflow"),
+		Type:  asttest.NewTypeId("Exception"),
+	}
+	typeDeclEMathError := &ast.TypeDecl{
+		Ident: asttest.NewIdent("EMathError"),
+		Type:  asttest.NewTypeId("Exception"),
+	}
+
+	runProgram(t,
+		"with 1 ExceptionBlockHandler", true,
+		[]rune(`PROGRAM OneExceptionBlockHandler;
+type
+	EZeroDivide = Exception;
+begin
+	try
+		X := Y/Z;
+	except
+		on EZeroDivide do HandleZeroDivide;
+	end;
+end.
 `),
-		&ast.Statement{
-			Body: &ast.RepeatStmt{
-				StmtList: ast.StmtList{
-					{
-						Body: &ast.TryExceptStmt{
-							Statements: ast.StmtList{
-								{
-									Body: &ast.AssignStatement{
-										Designator: asttest.NewDesignator("X"),
-										Expression: asttest.NewExpression(
-											&ast.Term{
-												Factor: asttest.NewDesignatorFactor(asttest.NewIdent("Y")),
-												MulOpFactors: []*ast.MulOpFactor{
-													{MulOp: "/", Factor: asttest.NewDesignatorFactor(asttest.NewIdent("Z"))},
-												},
-											},
+		stateToProgram("OneExceptionBlockHandler",
+			ast.DeclSections{
+				ast.TypeSection{typeDeclEZeroDivide},
+			},
+			&ast.Statement{
+				Body: &ast.TryExceptStmt{
+					Statements: ast.StmtList{
+						{
+							Body: &ast.AssignStatement{
+								Designator: asttest.NewDesignator("X"),
+								Expression: asttest.NewExpression(
+									&ast.Term{
+										Factor: asttest.NewDesignatorFactor(asttest.NewIdent("Y")),
+										MulOpFactors: []*ast.MulOpFactor{
+											{MulOp: "/", Factor: asttest.NewDesignatorFactor(asttest.NewIdent("Z"))},
+										},
+									},
+								),
+							},
+						},
+					},
+					ExceptionBlock: &ast.ExceptionBlock{
+						Handlers: ast.ExceptionBlockHandlers{
+							{
+								Type: asttest.NewTypeId(
+									"EZeroDivide",
+									astcore.NewDeclaration(typeDeclEZeroDivide.Ident, typeDeclEZeroDivide),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator(
+											asttest.NewQualId("HandleZeroDivide"),
 										),
 									},
 								},
 							},
-							ExceptionBlock: &ast.ExceptionBlock{
-								Handlers: ast.ExceptionBlockHandlers{
-									{
-										Type: asttest.NewTypeId("EZeroDivide"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleZeroDivide"),
-											},
-										},
-									},
-								},
-							},
 						},
 					},
 				},
 			},
-		},
+		),
 	)
 
-	run(
-		"with 3 ExceptionBlockHandlers without else",
-		[]rune(`
-try
-	DoSomething;
-except
-	on EZeroDivide do HandleZeroDivide;
-	on EOverflow do HandleOverflow;
-	on EMathError do HandleMathError; 
-end;
+	runProgram(t,
+		"with 3 ExceptionBlockHandlers without else", true,
+		[]rune(`PROGRAM ThreeExceptionBlockHandlersWithoutElse;
+type
+	EZeroDivide = Exception;
+	EOverflow = Exception;
+	EMathError = Exception;
+begin
+	try
+		DoSomething;
+	except
+		on EZeroDivide do HandleZeroDivide;
+		on EOverflow do HandleOverflow;
+		on EMathError do HandleMathError; 
+	end;
+end.
 `),
-		&ast.Statement{
-			Body: &ast.RepeatStmt{
-				StmtList: ast.StmtList{
-					{
-						Body: &ast.TryExceptStmt{
-							Statements: ast.StmtList{
-								{
+		stateToProgram("ThreeExceptionBlockHandlersWithoutElse",
+			ast.DeclSections{
+				ast.TypeSection{
+					typeDeclEZeroDivide,
+					typeDeclEOverflow,
+					typeDeclEMathError,
+				},
+			},
+			&ast.Statement{
+				Body: &ast.TryExceptStmt{
+					Statements: ast.StmtList{
+						{
+							Body: &ast.CallStatement{
+								Designator: asttest.NewDesignator("DoSomething"),
+							},
+						},
+					},
+					ExceptionBlock: &ast.ExceptionBlock{
+						Handlers: ast.ExceptionBlockHandlers{
+							{
+								Type: asttest.NewTypeId(
+									"EZeroDivide",
+									astcore.NewDeclaration(typeDeclEZeroDivide.Ident, typeDeclEZeroDivide),
+								),
+								Statement: &ast.Statement{
 									Body: &ast.CallStatement{
-										Designator: asttest.NewDesignator("DoSomething"),
+										Designator: asttest.NewDesignator("HandleZeroDivide"),
 									},
 								},
 							},
-							ExceptionBlock: &ast.ExceptionBlock{
-								Handlers: ast.ExceptionBlockHandlers{
-									{
-										Type: asttest.NewTypeId("EZeroDivide"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleZeroDivide"),
-											},
-										},
+							{
+								Type: asttest.NewTypeId(
+									"EOverflow",
+									astcore.NewDeclaration(typeDeclEOverflow.Ident, typeDeclEOverflow),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator("HandleOverflow"),
 									},
-									{
-										Type: asttest.NewTypeId("EOverflow"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleOverflow"),
-											},
-										},
-									},
-									{
-										Type: asttest.NewTypeId("EMathError"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleMathError"),
-											},
-										},
+								},
+							},
+							{
+								Type: asttest.NewTypeId(
+									"EMathError",
+									astcore.NewDeclaration(typeDeclEMathError.Ident, typeDeclEMathError),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator("HandleMathError"),
 									},
 								},
 							},
@@ -152,124 +204,155 @@ end;
 					},
 				},
 			},
-		},
+		),
 	)
 
-	run(
-		"with 3 ExceptionBlockHandlers with else",
-		[]rune(`
-try
-	DoSomething;
-except
-	on EZeroDivide do HandleZeroDivide;
-	on EOverflow do HandleOverflow;
-	on EMathError do HandleMathError;
-else
-	HandleAllOthers;	
-end;
+	runProgram(t,
+		"with 3 ExceptionBlockHandlers with else", true,
+		[]rune(`PROGRAM ThreeExceptionBlockHandlersWithElse;
+type
+	EZeroDivide = Exception;
+	EOverflow = Exception;
+	EMathError = Exception;
+begin
+	try
+		DoSomething;
+	except
+		on EZeroDivide do HandleZeroDivide;
+		on EOverflow do HandleOverflow;
+		on EMathError do HandleMathError;
+	else
+		HandleAllOthers;
+	end;
+end.
 `),
-		&ast.Statement{
-			Body: &ast.RepeatStmt{
-				StmtList: ast.StmtList{
-					{
-						Body: &ast.TryExceptStmt{
-							Statements: ast.StmtList{
-								{
+		stateToProgram("ThreeExceptionBlockHandlersWithElse",
+			ast.DeclSections{
+				ast.TypeSection{
+					typeDeclEZeroDivide,
+					typeDeclEOverflow,
+					typeDeclEMathError,
+				},
+			},
+			&ast.Statement{
+				Body: &ast.TryExceptStmt{
+					Statements: ast.StmtList{
+						{
+							Body: &ast.CallStatement{
+								Designator: asttest.NewDesignator("DoSomething"),
+							},
+						},
+					},
+					ExceptionBlock: &ast.ExceptionBlock{
+						Handlers: ast.ExceptionBlockHandlers{
+							{
+								Type: asttest.NewTypeId(
+									"EZeroDivide",
+									astcore.NewDeclaration(typeDeclEZeroDivide.Ident, typeDeclEZeroDivide),
+								),
+								Statement: &ast.Statement{
 									Body: &ast.CallStatement{
-										Designator: asttest.NewDesignator("DoSomething"),
+										Designator: asttest.NewDesignator("HandleZeroDivide"),
 									},
 								},
 							},
-							ExceptionBlock: &ast.ExceptionBlock{
-								Handlers: ast.ExceptionBlockHandlers{
-									{
-										Type: asttest.NewTypeId("EZeroDivide"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleZeroDivide"),
-											},
-										},
-									},
-									{
-										Type: asttest.NewTypeId("EOverflow"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleOverflow"),
-											},
-										},
-									},
-									{
-										Type: asttest.NewTypeId("EMathError"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("HandleMathError"),
-											},
-										},
+							{
+								Type: asttest.NewTypeId(
+									"EOverflow",
+									astcore.NewDeclaration(typeDeclEOverflow.Ident, typeDeclEOverflow),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator("HandleOverflow"),
 									},
 								},
-								Else: ast.StmtList{
-									{
-										Body: &ast.AssignStatement{
-											Designator: asttest.NewDesignator("HandleAllOthers"),
-										},
+							},
+							{
+								Type: asttest.NewTypeId(
+									"EMathError",
+									astcore.NewDeclaration(typeDeclEMathError.Ident, typeDeclEMathError),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator("HandleMathError"),
 									},
+								},
+							},
+						},
+						Else: ast.StmtList{
+							{
+								Body: &ast.CallStatement{
+									Designator: asttest.NewDesignator("HandleAllOthers"),
 								},
 							},
 						},
 					},
 				},
 			},
-		},
+		),
 	)
 
-	run(
+	typeDeclException := &ast.TypeDecl{
+		Ident: asttest.NewIdent("Exception"),
+		Type:  asttest.NewTypeId("Error"),
+	}
+
+	runProgram(t,
 		"with 1 ExceptionBlockHandler with ident",
-		[]rune(`
-try
-	DoSomething;
-except
-	on E: Exception do ErrorDialog(E.Message, E.HelpContext);
-end;
+		true,
+		[]rune(`PROGRAM ExceptionBlockHandlerWithIdent;
+type Exception = Error;
+begin
+	try
+		DoSomething;
+	except
+		on E: Exception do ErrorDialog(E.Message1, E.HelpContext); // TODO Use Message instead of Message1
+	end;
+end.
 `),
-		&ast.Statement{
-			Body: &ast.RepeatStmt{
-				StmtList: ast.StmtList{
-					{
-						Body: &ast.TryExceptStmt{
-							Statements: ast.StmtList{
-								{
-									Body: &ast.CallStatement{
-										Designator: asttest.NewDesignator("DoSomething"),
-									},
-								},
+		stateToProgram("ExceptionBlockHandlerWithIdent",
+			ast.DeclSections{
+				ast.TypeSection{
+					typeDeclException,
+				},
+			},
+			&ast.Statement{
+				Body: &ast.TryExceptStmt{
+					Statements: ast.StmtList{
+						{
+							Body: &ast.CallStatement{
+								Designator: asttest.NewDesignator("DoSomething"),
 							},
-							ExceptionBlock: &ast.ExceptionBlock{
-								Handlers: ast.ExceptionBlockHandlers{
-									{
-										Ident: asttest.NewIdent("E"),
-										Type:  asttest.NewTypeId("Exception"),
-										Statement: &ast.Statement{
-											Body: &ast.CallStatement{
-												Designator: asttest.NewDesignator("ErrorDialog"),
-												ExprList: ast.ExprList{
-													asttest.NewExpression(
-														&ast.Designator{
-															QualId: asttest.NewQualId("E"),
-															Items: ast.DesignatorItems{
-																asttest.NewDesignatorItemIdent("Message"),
-															},
-														},
-													),
-													asttest.NewExpression(
-														&ast.Designator{
-															QualId: asttest.NewQualId("E"),
-															Items: ast.DesignatorItems{
-																asttest.NewDesignatorItemIdent("HelpContext"),
-															},
-														},
-													),
+						},
+					},
+					ExceptionBlock: &ast.ExceptionBlock{
+						Handlers: ast.ExceptionBlockHandlers{
+							{
+								Ident: asttest.NewIdent("E"),
+								Type: asttest.NewTypeId(
+									"Exception",
+									astcore.NewDeclaration(typeDeclException.Ident, typeDeclException),
+								),
+								Statement: &ast.Statement{
+									Body: &ast.CallStatement{
+										Designator: asttest.NewDesignator("ErrorDialog"),
+										ExprList: ast.ExprList{
+											asttest.NewExpression(
+												&ast.Designator{
+													QualId: asttest.NewQualId("E"), // TODO have astcore.Declarations to E
+													Items: ast.DesignatorItems{
+														asttest.NewDesignatorItemIdent("Message1"),
+													},
 												},
-											},
+											),
+											asttest.NewExpression(
+												&ast.Designator{
+													QualId: asttest.NewQualId("E"), // TODO have astcore.Declarations to E
+													Items: ast.DesignatorItems{
+														asttest.NewDesignatorItemIdent("HelpContext"),
+													},
+												},
+											),
 										},
 									},
 								},
@@ -278,6 +361,6 @@ end;
 					},
 				},
 			},
-		},
+		),
 	)
 }
