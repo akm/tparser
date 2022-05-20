b5b4ff7af956e90655915c09f46dbca43070aa2d
akm <akm2000@gmail.com>
2022-05-20 23:50:47 +0900

    :green_heart: Fix ident locations

--- parser/unit_test/load_units_test/example1_test.go
+++ parser/unit_test/load_units_test/example1_test.go
@@ -31,22 +31,22 @@ func TestLoadExample1Project(t *testing.T) {
 	declInc := &ast.ExportedHeading{
 		FunctionHeading: &ast.FunctionHeading{
 			Type:  ast.FtProcedure,
-			Ident: asttest.NewIdent("Inc", asttest.NewIdentLocation(5, 12, 36, 15)),
+			Ident: asttest.NewIdent("Inc", asttest.NewIdentLocation(5, 11, 36, 14)),
 		},
 	}
 	declGet := &ast.ExportedHeading{
 		FunctionHeading: &ast.FunctionHeading{
 			Type:       ast.FtFunction,
-			Ident:      asttest.NewIdent("Get", asttest.NewIdentLocation(6, 11, 51, 14)),
-			ReturnType: asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(6, 16, 56, 23))),
+			Ident:      asttest.NewIdent("Get", asttest.NewIdentLocation(6, 10, 51, 13)),
+			ReturnType: asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(6, 15, 56, 22))),
 		},
 	}
 
 	declCount := &ast.VarDecl{
 		IdentList: asttest.NewIdentList(
-			asttest.NewIdent("Count", asttest.NewIdentLocation(10, 6, 90, 11)),
+			asttest.NewIdent("Count", asttest.NewIdentLocation(10, 5, 90, 10)),
 		),
-		Type:      asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(10, 13, 97, 20))),
+		Type:      asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(10, 12, 97, 19))),
 		ConstExpr: asttest.NewConstExpr(asttest.NewNumber("0")),
 	}
 
@@ -62,7 +62,7 @@ func TestLoadExample1Project(t *testing.T) {
 				&ast.FunctionDecl{
 					FunctionHeading: &ast.FunctionHeading{
 						Type:  ast.FtProcedure,
-						Ident: asttest.NewIdent("Inc", asttest.NewIdentLocation(12, 12, 123, 15)),
+						Ident: asttest.NewIdent("Inc", asttest.NewIdentLocation(12, 11, 123, 14)),
 					},
 					Block: &ast.Block{
 						Body: &ast.CompoundStmt{
@@ -71,7 +71,7 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.AssignStatement{
 										Designator: asttest.NewDesignator(
 											asttest.NewQualId(
-												asttest.NewIdent("Count", asttest.NewIdentLocation(14, 5, 139, 10)),
+												asttest.NewIdent("Count", asttest.NewIdentLocation(14, 4, 139, 9)),
 												astcore.NewDeclaration(declCount.IdentList[0], declCount),
 											),
 										),
@@ -79,7 +79,7 @@ func TestLoadExample1Project(t *testing.T) {
 											&ast.SimpleExpression{
 												Term: &ast.Term{Factor: asttest.NewDesignatorFactor(
 													asttest.NewQualId(
-														asttest.NewIdent("Count", asttest.NewIdentLocation(14, 14, 148, 19)),
+														asttest.NewIdent("Count", asttest.NewIdentLocation(14, 13, 148, 18)),
 														astcore.NewDeclaration(declCount.IdentList[0], declCount),
 													),
 												)},
@@ -97,8 +97,8 @@ func TestLoadExample1Project(t *testing.T) {
 				&ast.FunctionDecl{
 					FunctionHeading: &ast.FunctionHeading{
 						Type:       ast.FtFunction,
-						Ident:      asttest.NewIdent("Get", asttest.NewIdentLocation(17, 11, 214, 14)),
-						ReturnType: asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(17, 16, 219, 23))),
+						Ident:      asttest.NewIdent("Get", asttest.NewIdentLocation(17, 10, 214, 13)),
+						ReturnType: asttest.NewOrdIdent(asttest.NewIdent("Integer", asttest.NewIdentLocation(17, 15, 219, 22))),
 					},
 					Block: &ast.Block{
 						Body: &ast.CompoundStmt{
@@ -107,12 +107,13 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.AssignStatement{
 										Designator: asttest.NewDesignator(
 											asttest.NewQualId(
-												asttest.NewIdent("Result", asttest.NewIdentLocation(19, 5, 239, 11)),
+												asttest.NewIdent("Result", asttest.NewIdentLocation(19, 4, 239, 10)),
 											),
 										),
 										Expression: asttest.NewExpression(
 											asttest.NewQualId(
-												asttest.NewIdent("Count", asttest.NewIdentLocation(19, 15, 249, 20)),
+												asttest.NewIdent("Count",
+													asttest.NewIdentLocation(19, 14, 249, 19)),
 												astcore.NewDeclaration(declCount.IdentList[0], declCount),
 											),
 										),
@@ -125,6 +126,7 @@ func TestLoadExample1Project(t *testing.T) {
 		},
 	}
 
+	actualUnitBar.DeclarationMap = nil
 	t.Run("bar.pas", func(t *testing.T) {
 		if !assert.Equal(t, expectedUnitBar, actualUnitBar) {
 			asttest.AssertUnit(t, expectedUnitBar, actualUnitBar)
@@ -134,7 +136,7 @@ func TestLoadExample1Project(t *testing.T) {
 	declProcess := &ast.ExportedHeading{
 		FunctionHeading: &ast.FunctionHeading{
 			Type:  ast.FtProcedure,
-			Ident: asttest.NewIdent("Process", asttest.NewIdentLocation(5, 12, 36, 19)),
+			Ident: asttest.NewIdent("Process", asttest.NewIdentLocation(5, 11, 36, 18)),
 		},
 	}
 
@@ -146,14 +148,14 @@ func TestLoadExample1Project(t *testing.T) {
 		},
 		ImplementationSection: &ast.ImplementationSection{
 			UsesClause: ast.UsesClause{
-				{Ident: asttest.NewIdent("SysUtils", asttest.NewIdentLocation(9, 7, 71, 15))},
-				{Ident: asttest.NewIdent("bar", asttest.NewIdentLocation(9, 17, 81, 20))},
+				{Ident: asttest.NewIdent("SysUtils", asttest.NewIdentLocation(9, 6, 71, 14))},
+				{Ident: asttest.NewIdent("bar", asttest.NewIdentLocation(9, 16, 81, 19))},
 			},
 			DeclSections: ast.DeclSections{
 				&ast.FunctionDecl{
 					FunctionHeading: &ast.FunctionHeading{
 						Type:  ast.FtProcedure,
-						Ident: asttest.NewIdent("Process", asttest.NewIdentLocation(11, 12, 99, 19)),
+						Ident: asttest.NewIdent("Process", asttest.NewIdentLocation(11, 11, 99, 18)),
 					},
 					Block: &ast.Block{
 						Body: &ast.CompoundStmt{
@@ -162,8 +164,7 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.CallStatement{
 										Designator: asttest.NewDesignator(
 											asttest.NewQualId(
-												asttest.NewIdent("Inc", asttest.NewIdentLocation(13, 5, 119, 8)),
-												astcore.NewDeclaration(declInc.Ident, declInc),
+												asttest.NewIdentRef("Inc", asttest.NewIdentLocation(13, 4, 119, 7), astcore.NewDeclaration(declInc.Ident, declInc)),
 											),
 										),
 									},
@@ -172,7 +173,7 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.CallStatement{
 										Designator: asttest.NewDesignator(
 											asttest.NewQualId(
-												asttest.NewIdent("Writeln", asttest.NewIdentLocation(14, 5, 128, 12)),
+												asttest.NewIdent("Writeln", asttest.NewIdentLocation(14, 4, 128, 11)),
 											),
 										),
 										ExprList: ast.ExprList{
@@ -180,8 +181,8 @@ func TestLoadExample1Project(t *testing.T) {
 												&ast.DesignatorFactor{
 													Designator: asttest.NewDesignator(
 														&ast.QualId{
-															UnitId: asttest.NewIdentRef("bar", asttest.NewIdentLocation(14, 14, 137, 17)),
-															Ident:  asttest.NewIdentRef("Get", asttest.NewIdentLocation(14, 18, 141, 21)),
+															UnitId: asttest.NewIdentRef("bar", asttest.NewIdentLocation(14, 13, 137, 16), astcore.NewDeclaration(expectedUnitBar.Ident, expectedUnitBar)),
+															Ident:  asttest.NewIdentRef("Get", asttest.NewIdentLocation(14, 17, 141, 20), astcore.NewDeclaration(declGet.Ident, declGet)),
 														},
 													),
 												},
@@ -193,8 +194,8 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.CallStatement{
 										Designator: asttest.NewDesignator(
 											&ast.QualId{
-												UnitId: asttest.NewIdentRef("bar", asttest.NewIdentLocation(15, 5, 152, 8)),
-												Ident:  asttest.NewIdentRef("Inc", asttest.NewIdentLocation(15, 9, 156, 12), astcore.NewDeclaration(declInc.Ident, declInc)),
+												UnitId: asttest.NewIdentRef("bar", asttest.NewIdentLocation(15, 4, 152, 7), astcore.NewDeclaration(expectedUnitBar.Ident, expectedUnitBar)),
+												Ident:  asttest.NewIdentRef("Inc", asttest.NewIdentLocation(15, 8, 156, 11), astcore.NewDeclaration(declInc.Ident, declInc)),
 											},
 										),
 									},
@@ -203,7 +204,7 @@ func TestLoadExample1Project(t *testing.T) {
 									Body: &ast.CallStatement{
 										Designator: asttest.NewDesignator(
 											asttest.NewQualId(
-												asttest.NewIdent("Writeln", asttest.NewIdentLocation(16, 5, 165, 12)),
+												asttest.NewIdent("Writeln", asttest.NewIdentLocation(16, 4, 165, 11)),
 											),
 										),
 										ExprList: ast.ExprList{
@@ -211,8 +212,8 @@ func TestLoadExample1Project(t *testing.T) {
 												&ast.DesignatorFactor{
 													Designator: asttest.NewDesignator(
 														asttest.NewQualId(
-															(*ast.UnitId)(asttest.NewIdent("bar", asttest.NewIdentLocation(16, 14, 174, 17))),
-															asttest.NewIdent("Get", asttest.NewIdentLocation(16, 18, 178, 21)),
+															asttest.NewIdentRef("bar", asttest.NewIdentLocation(16, 13, 174, 16), astcore.NewDeclaration(expectedUnitBar.Ident, expectedUnitBar)),
+															asttest.NewIdentRef("Get", asttest.NewIdentLocation(16, 17, 178, 20), astcore.NewDeclaration(declGet.Ident, declGet)),
 														),
 													),
 												},
@@ -228,6 +229,7 @@ func TestLoadExample1Project(t *testing.T) {
 		},
 	}
 
+	actualUnitFoo.DeclarationMap = nil
 	t.Run("foo.pas", func(t *testing.T) {
 		if !assert.Equal(t, expectedUnitFoo, actualUnitFoo) {
 			asttest.AssertUnit(t, expectedUnitFoo, actualUnitFoo)
@@ -240,13 +242,13 @@ func TestLoadExample1Project(t *testing.T) {
 			Ident: asttest.NewIdent("example1", asttest.NewIdentLocation(1, 9, 8, 17)),
 			ProgramBlock: &ast.ProgramBlock{
 				UsesClause: ast.UsesClause{
-					{Ident: asttest.NewIdent("SysUtils", asttest.NewIdentLocation(4, 4, 47, 12))},
+					{Ident: asttest.NewIdent("SysUtils", asttest.NewIdentLocation(4, 3, 47, 11))},
 					{
-						Ident: asttest.NewIdent("foo", asttest.NewIdentLocation(5, 4, 60, 7)),
+						Ident: asttest.NewIdent("foo", asttest.NewIdentLocation(5, 3, 60, 6)),
 						Path:  ext.StringPtr("'foo.pas'"),
 					},
 					{
-						Ident: asttest.NewIdent("bar", asttest.NewIdentLocation(6, 4, 81, 7)),
+						Ident: asttest.NewIdent("bar", asttest.NewIdentLocation(6, 3, 81, 6)),
 						Path:  ext.StringPtr("'subdir1\\bar.pas'"),
 					},
 				},
@@ -257,8 +259,8 @@ func TestLoadExample1Project(t *testing.T) {
 								Body: &ast.CallStatement{
 									Designator: asttest.NewDesignator(
 										&ast.QualId{
-											UnitId: asttest.NewIdentRef("foo", asttest.NewIdentLocation(9, 5, 120, 8)),
-											Ident:  asttest.NewIdentRef("Process", asttest.NewIdentLocation(9, 9, 124, 16), astcore.NewDeclaration(declProcess.Ident, declProcess)),
+											UnitId: asttest.NewIdentRef("foo", asttest.NewIdentLocation(9, 4, 120, 7), astcore.NewDeclaration(expectedUnitFoo.Ident, expectedUnitFoo)),
+											Ident:  asttest.NewIdentRef("Process", asttest.NewIdentLocation(9, 8, 124, 15), astcore.NewDeclaration(declProcess.Ident, declProcess)),
 										},
 									),
 								},
@@ -267,7 +269,7 @@ func TestLoadExample1Project(t *testing.T) {
 								Body: &ast.CallStatement{
 									Designator: asttest.NewDesignator(
 										asttest.NewQualId(
-											asttest.NewIdent("Readln", asttest.NewIdentLocation(10, 5, 137, 11)),
+											asttest.NewIdent("Readln", asttest.NewIdentLocation(10, 4, 137, 10)),
 										),
 									),
 								},
