97e271c0b104885e22bf225522519b85a9f97e24
akm <akm2000@gmail.com>
2022-05-29 17:35:50 +0900

    :green_heart: Add uses in implementation section for test

--- parser/parsertest/unit_test/same_ident_test/Unit2.pas
+++ parser/parsertest/unit_test/same_ident_test/Unit2.pas
@@ -8,22 +8,28 @@ var
   Unit1: string = 'Unit1@Unit2';
   Project1: string = 'Project1@Unit2';
 
-
 implementation
 
+uses
+  Unit1;
 
 initialization
   Writeln('-- Unit2 Initialization----');
   Writeln(Foo);
   Writeln(Bar);
-  Writeln(Unit1);
+  // Writeln(Unit1);
+  Writeln(Unit1.Foo);
+  Writeln(Unit1.Bar);
+  // Writeln(Unit1.Unit1.Foo);
+
 
 finalization
   Writeln('-- Unit2 Finalization----');
   Writeln(Foo);
   Writeln(Bar);
-  Writeln(Unit1);
+  // Writeln(Unit1);
+  Writeln(Unit1.Foo);
+  Writeln(Unit1.Bar);
   Readln;
 
 end.
- 
