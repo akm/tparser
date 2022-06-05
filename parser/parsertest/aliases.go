ebc8df2f529bca7a36e95826deea022d75c3132f
akm <akm2000@gmail.com>
2022-06-05 14:59:59 +0900

    :green_heart: :recycle: Move base.go to runners/base.go and add aliases.go to parsertest

---
+++ parser/parsertest/aliases.go
@@ -0,0 +1,13 @@
+package parsertest
+
+import (
+	"github.com/akm/tparser/parser/parsertest/runners"
+)
+
+var (
+	NewTestParser         = runners.NewTestParser
+	NewTestProgramParser  = runners.NewTestProgramParser
+	NewTestUnitParser     = runners.NewTestUnitParser
+	NewTestProgramContext = runners.NewTestProgramContext
+	NewTestUnitContext    = runners.NewTestUnitContext
+)
