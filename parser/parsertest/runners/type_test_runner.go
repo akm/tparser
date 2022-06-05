b4bd5e876ca4cbae3b06734fd56efb255bfed595
akm <akm2000@gmail.com>
2022-06-05 15:25:34 +0900

    :green_heart: :recycle: Capitalize TypeTestRunner fields

--- parser/parsertest/runners/type_test_runner.go
+++ parser/parsertest/runners/type_test_runner.go
@@ -11,19 +11,19 @@ import (
 
 type TypeTestRunner struct {
 	t        *testing.T
-	name     string
-	text     *[]rune
-	expected ast.Type
-	funcs    []func() interface{}
+	Name     string
+	Text     *[]rune
+	Expected ast.Type
+	Funcs    []func() interface{}
 }
 
 func NewTypeTestRunner(t *testing.T, name string, text []rune, expected ast.Type, funcs ...func() interface{}) *TypeTestRunner {
-	return &TypeTestRunner{t: t, name: name, text: &text, expected: expected, funcs: funcs}
+	return &TypeTestRunner{t: t, Name: name, Text: &text, Expected: expected, Funcs: funcs}
 }
 
 func (tt *TypeTestRunner) newParser(text *[]rune) *parser.Parser {
-	args := make([]interface{}, len(tt.funcs))
-	for i, f := range tt.funcs {
+	args := make([]interface{}, len(tt.Funcs))
+	for i, f := range tt.Funcs {
 		args[i] = f()
 	}
 	r := NewTestParser(text, args...)
@@ -32,21 +32,21 @@ func (tt *TypeTestRunner) newParser(text *[]rune) *parser.Parser {
 }
 
 func (tt *TypeTestRunner) Run() *TypeTestRunner {
-	tt.t.Run(tt.name, func(t *testing.T) {
-		p := tt.newParser(tt.text)
+	tt.t.Run(tt.Name, func(t *testing.T) {
+		p := tt.newParser(tt.Text)
 		res, err := p.ParseType()
 		if assert.NoError(t, err) {
 			asttest.ClearLocations(t, res)
-			assert.Equal(t, tt.expected, res)
+			assert.Equal(t, tt.Expected, res)
 		}
 	})
 	return tt
 }
 
 func (tt *TypeTestRunner) RunTypeSection(declName string) *TypeTestRunner {
-	tt.t.Run(tt.name+" in type section", func(t *testing.T) {
-		expectedSection := ast.TypeSection{{Ident: asttest.NewIdent(declName), Type: tt.expected}}
-		sectionStr := "type " + declName + " = " + string(*tt.text) + ";"
+	tt.t.Run(tt.Name+" in type section", func(t *testing.T) {
+		expectedSection := ast.TypeSection{{Ident: asttest.NewIdent(declName), Type: tt.Expected}}
+		sectionStr := "type " + declName + " = " + string(*tt.Text) + ";"
 		sectionRunes := []rune(sectionStr)
 		p := tt.newParser(&sectionRunes)
 		res, err := p.ParseTypeSection(true)
@@ -59,9 +59,9 @@ func (tt *TypeTestRunner) RunTypeSection(declName string) *TypeTestRunner {
 }
 
 func (tt *TypeTestRunner) RunVarSection(declName string) *TypeTestRunner {
-	tt.t.Run(tt.name+" in var section", func(t *testing.T) {
-		expectedSection := ast.VarSection{{IdentList: asttest.NewIdentList(declName), Type: tt.expected}}
-		sectionStr := "var " + declName + ": " + string(*tt.text) + ";"
+	tt.t.Run(tt.Name+" in var section", func(t *testing.T) {
+		expectedSection := ast.VarSection{{IdentList: asttest.NewIdentList(declName), Type: tt.Expected}}
+		sectionStr := "var " + declName + ": " + string(*tt.Text) + ";"
 		sectionRunes := []rune(sectionStr)
 		p := tt.newParser(&sectionRunes)
 		res, err := p.ParseVarSection(true)
